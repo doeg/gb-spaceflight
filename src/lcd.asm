a7b94c076c288b96c47ddbde4b9070eeeb7b4fef
Sara Bee <hi@doeg.gy>
2017-12-26 20:17:02 -0500

    Whitespace

--- src/lcd.asm
+++ src/lcd.asm
@@ -5,31 +5,31 @@ SECTION "lcd", ROMX
 INCLUDE "constants.inc"
 
 lcd_off::
-    ld HL, LCD_CTRL
-    res 7, [HL]
-    ret
+  ld HL, LCD_CTRL
+  res 7, [HL]
+  ret
 
 lcd_on::
-    ld HL, LCD_CTRL
-    set 7, [HL]
-    ret
+  ld HL, LCD_CTRL
+  set 7, [HL]
+  ret
 
 wait_vblank::
-    ld A, [LCD_LINE_Y]
-    cp 144
-    jr nz, wait_vblank
-    ret
+  ld A, [LCD_LINE_Y]
+  cp 144
+  jr nz, wait_vblank
+  ret
 
 clear_bg_map::
-    ld HL, $9C00
+  ld HL, $9C00
 
 .clear_loop:
-    ld [HL], B
-    inc HL
-    ld A, H
-    cp $9F
-    jr nz, .clear_loop
-    ld A, L
-    cp $FF
-    jr nz, .clear_loop
-    ret
+  ld [HL], B
+  inc HL
+  ld A, H
+  cp $9F
+  jr nz, .clear_loop
+  ld A, L
+  cp $FF
+  jr nz, .clear_loop
+  ret
