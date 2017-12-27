bbe77266819e72466c00504ea4952e529a6ef286
Sara Bee <hi@doeg.gy>
2017-12-27 16:50:36 -0500

    Load the whole background

--- src/game.asm
+++ src/game.asm
@@ -16,6 +16,7 @@ handle_game_timer_interrupt::
   jp nz, .finish
 
   ; If zero, move the window and reset the counter
+  call wait_vblank
   call motion_update
   ld a, DEFAULT_INTERRUPT_COUNTER
 
@@ -27,10 +28,11 @@ handle_game_timer_interrupt::
 motion_update::
   push af
   ld a, [LCD_SCROLL_Y]
-  inc a
-  inc a
-  inc a
-  inc a
+  dec a
+  dec a
+  dec a
+  dec a
+  dec a
   ld [LCD_SCROLL_Y], a
   pop af
   ret
@@ -57,157 +59,12 @@ load_game_data::
   ld BC, bg_space_tile_data
   ld HL, VRAM_TILES_BACKGROUND
   call memcpy
-  ; Load tile data
-  call .load_menu_tiles_0
-  call .load_menu_tiles_1
+  call .load_all_tiles
   ret
-.load_menu_tiles_0:
-  ; We just load every line by hand.
-  ld DE, $14
-  ld BC, bg_space_map_data + ($14 * $0)
-  ld HL, $9C00
-  call memcpy
-  ld DE, $14
-  ld BC, bg_space_map_data + ($14 * $1)
-  ld HL, $9C20
-  call memcpy
-  ld DE, $14
-  ld BC, bg_space_map_data + ($14 * $2)
-  ld HL, $9C40
-  call memcpy
-  ld DE, $14
-  ld BC, bg_space_map_data + ($14 * $3)
-  ld HL, $9C60
-  call memcpy
-  ld DE, $14
-  ld BC, bg_space_map_data + ($14 * $4)
-  ld HL, $9C80
-  call memcpy
-  ld DE, $14
-  ld BC, bg_space_map_data + ($14 * $5)
-  ld HL, $9CA0
-  call memcpy
-  ld DE, $14
-  ld BC, bg_space_map_data + ($14 * $6)
-  ld HL, $9CC0
-  call memcpy
-  ld DE, $14
-  ld BC, bg_space_map_data + ($14 * $7)
-  ld HL, $9CE0
-  call memcpy
-  ld DE, $14
-  ld BC, bg_space_map_data + ($14 * $8)
-  ld HL, $9D00
-  call memcpy
-  ld DE, $14
-  ld BC, bg_space_map_data + ($14 * $9)
-  ld HL, $9D20
-  call memcpy
-  ld DE, $14
-  ld BC, bg_space_map_data + ($14 * $A)
-  ld HL, $9D40
-  call memcpy
-  ld DE, $14
-  ld BC, bg_space_map_data + ($14 * $B)
-  ld HL, $9D60
-  call memcpy
-  ld DE, $14
-  ld BC, bg_space_map_data + ($14 * $C)
-  ld HL, $9D80
-  call memcpy
-  ld DE, $14
-  ld BC, bg_space_map_data + ($14 * $D)
-  ld HL, $9DA0
-  call memcpy
-  ld DE, $14
-  ld BC, bg_space_map_data + ($14 * $E)
-  ld HL, $9DC0
-  call memcpy
-  ld DE, $14
-  ld BC, bg_space_map_data + ($14 * $F)
-  ld HL, $9DE0
-  call memcpy
-  ld DE, $14
-  ld BC, bg_space_map_data + ($14 * $10)
-  ld HL, $9E00
-  call memcpy
-  ld DE, $14
-  ld BC, bg_space_map_data + ($14 * $11)
-  ld HL, $9E20
-  call memcpy
-  ret
-.load_menu_tiles_1:
-  ; We just load every line by hand.
-  ld DE, $0E
-  ld BC, bg_space_map_data + ($14 * $0)
-  ld HL, $9C13
-  call memcpy
-  ld DE, $0E
-  ld BC, bg_space_map_data + ($14 * $1)
-  ld HL, $9C33
-  call memcpy
-  ld DE, $0E
-  ld BC, bg_space_map_data + ($14 * $2)
-  ld HL, $9C53
-  call memcpy
-  ld DE, $0E
-  ld BC, bg_space_map_data + ($14 * $3)
-  ld HL, $9C73
-  call memcpy
-  ld DE, $0E
-  ld BC, bg_space_map_data + ($14 * $4)
-  ld HL, $9C93
-  call memcpy
-  ld DE, $0E
-  ld BC, bg_space_map_data + ($14 * $5)
-  ld HL, $9CB3
-  call memcpy
-  ld DE, $0E
-  ld BC, bg_space_map_data + ($14 * $6)
-  ld HL, $9CD3
-  call memcpy
-  ld DE, $0E
-  ld BC, bg_space_map_data + ($14 * $7)
-  ld HL, $9CF3
-  call memcpy
-  ld DE, $0E
-  ld BC, bg_space_map_data + ($14 * $8)
-  ld HL, $9D13
-  call memcpy
-  ld DE, $0E
-  ld BC, bg_space_map_data + ($14 * $9)
-  ld HL, $9D33
-  call memcpy
-  ld DE, $0E
-  ld BC, bg_space_map_data + ($14 * $A)
-  ld HL, $9D53
-  call memcpy
-  ld DE, $0E
-  ld BC, bg_space_map_data + ($14 * $B)
-  ld HL, $9D73
-  call memcpy
-  ld DE, $0E
-  ld BC, bg_space_map_data + ($14 * $C)
-  ld HL, $9D93
-  call memcpy
-  ld DE, $0E
-  ld BC, bg_space_map_data + ($14 * $D)
-  ld HL, $9DB3
-  call memcpy
-  ld DE, $0E
-  ld BC, bg_space_map_data + ($14 * $E)
-  ld HL, $9DD3
-  call memcpy
-  ld DE, $0E
-  ld BC, bg_space_map_data + ($14 * $F)
-  ld HL, $9DF3
-  call memcpy
-  ld DE, $0E
-  ld BC, bg_space_map_data + ($14 * $10)
-  ld HL, $9E13
-  call memcpy
-  ld DE, $0E
-  ld BC, bg_space_map_data + ($14 * $11)
-  ld HL, $9E33
+
+.load_all_tiles:
+  ld de, bg_space_tile_map_size ;len
+  ld bc, bg_space_map_data ;src
+  ld hl, $9C00 ;dest
   call memcpy
   ret
