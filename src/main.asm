bf0d9a64042e23b2780613fcfc172fb45ddc0db3
Sara Bee <hi@doeg.gy>
2017-12-30 12:17:38 -0500

    Add DMA transfer code & clear_oam subroutine

--- src/main.asm
+++ src/main.asm
@@ -19,6 +19,14 @@ SECTION  "start", ROM0[$0100]
 
 INCLUDE "header.inc"
 
+; Shadow OAM needs to be 16-byte aligned since
+; the last two hex digits of the address are
+; assumed to be 00 during DMA transfer.
+SECTION "variables", WRAM0, ALIGN[2]
+variables_start:
+SHADOW_OAM:: ds 40 * 4
+variables_end:
+
 SECTION "main", ROMX
 
 main::
@@ -32,19 +40,38 @@ main::
 
 ; Copies the DMA handler code to HRAM
 init_dma::
-  ld de, vblank_handler_end - vblank_handler + 1 ;block size
-  ld bc, vblank_handler ;source
-  ld hl, pVBLANK_HANDLER; dest
+  ; FIXME the final reti gets clipped unless a +1 is added.
+  ; I think this memcpy implementation has an off-by-one error...
+  ; at least, for how I want to use it.
+  ld de, vblank_handler_end - vblank_handler + 1
+  ld bc, vblank_handler
+  ld hl, pVBLANK_HANDLER
   call memcpy
   ret
 
 vblank_handler::
   push af
-  push hl
-  nop
-  nop
-  nop
-  pop hl
+  ld a, pSHADOW_OAM / $100
+  ; Invoke DMA
+  ldh [pOAM_DMA_TRANS], a
+  ; Delay for 28 (5 x 50) cycles (~200ms)
+  ld a, $28
+.dma_wait:
+  dec a
+  jr nz, .dma_wait
   pop af
   reti
 vblank_handler_end:
+
+clear_oam::
+  push af
+  push hl
+  push bc
+  xor a
+  ld hl, $FE00 ; start of OAM
+  ld bc, $A0 ; the full size of the OAM area: 40 bytes, 4 bytes per sprite
+  call mem_set
+  pop bc
+  pop hl
+  pop af 
+  ret
